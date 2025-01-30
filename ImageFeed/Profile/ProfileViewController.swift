6562fb47c0cb1c2516900f4fae6c2d9267e96c51
Mat <me@me.me>
2025-01-30 18:17:53 +0300

    Started doing profile screen

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Матвей Сюзев on 1/30/25.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    // MARK: - IB Outlets
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    @IBOutlet weak var exitButton: UIButton!
+    // MARK: - Public Properties
+
+    // MARK: - Private Properties
+
+    // MARK: - Initializers
+
+    // MARK: - Overrides Methods
+
+    // MARK: - IB Actions
+    @IBAction func didTapExitButton(_ sender: Any) {
+    }
+    // MARK: - Public Methods
+
+    // MARK: - Private Methods
+
+}
